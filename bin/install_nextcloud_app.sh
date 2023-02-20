da26315bf80600ec3f12c546893040bdb00d2276
Allen Chen <a3linux@gmail.com>
2023-02-20 22:51:45 +0800

    -Add Nextcloud apps installation

---
+++ bin/install_nextcloud_app.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+NC_CONTAINER_NAME="homecloud_nextcloudapp"
+# Source path
+mypath=$(realpath "${BASH_SOURCE:-$0}")
+MYSELF_PATH=$(dirname "${mypath}")
+NEXTCLOUD_APPS=$(cat "${MYSELF_PATH%%\/bin}"/etc/nextcloud.apps)
+
+for app in ${NEXTCLOUD_APPS}
+do
+    docker exec -i -u www-data "${NC_CONTAINER_NAME}" php /var/www/html/occ app:install "${app}"
+done
