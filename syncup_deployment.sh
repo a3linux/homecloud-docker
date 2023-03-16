ea3574cf422945aa92befc1aa13fc03d5cc8d28e
Allen Chen <a3linux@gmail.com>
2023-03-16 21:03:05 +0800

    -Add colourful output for syncup_deployment.sh script

--- syncup_deployment.sh
+++ syncup_deployment.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 # Generate HomeCloud service volumes(mount to docker)
 #
+declare -A colors
+colors[Color_Off]='\033[0m'
+colors[Red]='\033[0;31m'
+colors[Green]='\033[0;32m'
+colors[Yellow]='\033[0;33m'
+colors[Blue]='\033[0;34m'
+colors[Purple]='\033[0;35m'
+colors[Cyan]='\033[0;36m'
+colors[White]='\033[0;37m'
+
 SETUP_ENV_FULLPATH=""
 usage() {
-    echo "Usage: $0 -c some_path/homecloud.<env>"
-    echo "  Please provide the homecloud service environment file, e.g. some_path/homecloud.dev"
-    echo "  You COULD create such file based on templates/homecloud.env.template"
-    echo "  The filename should be homecloud.<env>, <env> indicates the deployment, can be dev | prod"
+    echo -e "Usage: ${colors[Cyan]}$0 -c some_path/homecloud.<env>${colors[Color_Off]}"
+    echo -e "  Please provide the homecloud service environment file, e.g. ${colors[Cyan]}<some_path>/homecloud.dev${colors[Color_Off]}"
+    echo -e "  Copy the ${colors[Red]}homecloud.env${colors[Color_Off]} to start, the filename should be ${colors[Purple]}homecloud.<env>${colors[Color_Off]}, <env> should be ${colors[Purple]}dev | prod${colors[Color_Off]}"
 }
 
 error_exit() {
@@ -43,7 +52,7 @@ fi
 source "${SETUP_ENV_FULLPATH}"
 
 if [ ! -d "${SERVICE_DESTINATION}" ]; then
-    echo "Folder ${SERVICE_DESTINATION} does not exist or can not access!!!"
+    echo -e "!!!Folder ${colors[Red]}${SERVICE_DESTINATION} ${colors[Color_Off]}does not exist or can not access!!!"
     error_exit
 fi
 
@@ -71,6 +80,10 @@ bin
 etc
 "
 
+echo ""
+echo -e "${colors[Green]}Start prepare the HomeCloud deployment environment: ${colors[Blue]}${TARGET_ENV} ${colors[Color_Off]}"
+echo ""
+
 for app in ${APPs}
 do
     mkdir -p "${APPS_BASE}"/"${app}"
@@ -94,10 +107,10 @@ MYSELF_PATH=$(dirname "${mypath}")
 HC_PROGRAM_PATH="${MYSELF_PATH%%\/bin}"
 HC_CONF_SOURCE_PATH="${HC_PROGRAM_PATH}"/conf
 
-echo "Copy scripts and data"
+echo -e "${colors[Cyan]}Copy global scripts, config files${colors[Color_Off]}"
 if [ ! -f "${SERVICE_DESTINATION}"/"${SETUP_ENV_FILENAME}" ]; then
     # if the setup file is not in destination folder, copy it
-    cp -v "${SETUP_ENV_FULLPATH}" "${SERVICE_DESTINATION}"/"${SETUP_ENV_FILENAME}"
+    cp -vf "${SETUP_ENV_FULLPATH}" "${SERVICE_DESTINATION}"/"${SETUP_ENV_FILENAME}"
 fi
 HC_DC_BIN_PATH="${SERVICE_DESTINATION}"/bin
 rsync -ar "${HC_PROGRAM_PATH}"/bin/ "${HC_DC_BIN_PATH}"/
@@ -113,13 +126,15 @@ sed -i -e "s|%SERVICE_DESTINATION%|${SERVICE_DESTINATION}|g" "${HC_DC_ETC_PATH}"
 sed -i -e "s|%DEPLOYMENT_USER%|${DEPLOYMENT_USER}|g" "${HC_DC_ETC_PATH}"/cron.hourly/homecloud_backup
 sed -i -e "s|%ALERT_EMAIL%|${ALERT_EMAIL}|g" "${HC_DC_ETC_PATH}"/cron.hourly/homecloud_backup
 chmod +x "${HC_DC_ETC_PATH}"/cron.hourly/homecloud_backup "${HC_DC_ETC_PATH}"/cron.daily/homecloud_backup
-cp -v "${HC_PROGRAM_PATH}"/docker-compose.yml "${SERVICE_DESTINATION}"/
-HC_DC_ENV_FILE="${SERVICE_DESTINATION}"/docker-compose.${TARGET_ENV}.yml
+cp -vf "${HC_PROGRAM_PATH}"/docker-compose.yml "${SERVICE_DESTINATION}"/
 
+# Generate the docker-compose.<env>.yml
+echo -e "${colors[Cyan]}Re-populate ${SERVICE_DESTINATION}/docker-compose.${TARGET_ENV}.yml${colors[Color_Off]}"
+HC_DC_ENV_FILE="${SERVICE_DESTINATION}"/docker-compose.${TARGET_ENV}.yml
+printf "services:\n" > "${HC_DC_ENV_FILE}"
 # Setup LB config files
-echo "Populate LB configurations ..."
+echo -e "${colors[Cyan]}Populate LB configurations ...${colors[Color_Off]}"
 mkdir -p "${APPS_BASE}"/lb/conf.d "${APPS_BASE}"/lb/certs
-echo "  Sync up config files."
 rsync -ar "${HC_CONF_SOURCE_PATH}"/lb/ "${APPS_BASE}"/lb/
 
 PRIMARY_CERT_PATH=/certs/${PRIMARY_CERT_FILE:=fullchain.pem}
@@ -129,7 +144,7 @@ AUTHENTIK_PRIVATE_KEY_PATH=/certs/${AUTHENTIK_PRIVATE_KEY_FILE:=private.pem}
 CODE_CERT_PATH=/certs/${PRIMARY_CERT_FILE:=fullchain.pem}
 CODE_PRIVATE_KEY_PATH=/certs/${PRIMARY_PRIVATE_KEY_FILE:=private.pem}
 
-echo "  Update config files."
+echo -e "  ${colors[Cyan]}Update config files${colors[Color_Off]}"
 for conf in "${APPS_BASE}"/lb/conf.d/*.conf
 do
     for find_to_replace in PRIMARY_SERVER_NAME AUTHENTIK_SERVER_NAME CODE_SERVER_NAME PRIMARY_CERT_PATH AUTHENTIK_CERT_PATH PRIMARY_PRIVATE_KEY_PATH AUTHENTIK_PRIVATE_KEY_PATH HTTPS_PORT CODE_CERT_PATH CODE_PRIVATE_KEY_PATH
@@ -142,9 +157,7 @@ if [ "${CODE_SERVER_ENABLED}" != "yes" ]; then
     rm -f "${APPS_BASE}"/lb/conf.d/*-code.conf
 fi
 
-echo "  Generate lb docker-compose config."
-# Generate the docker-compose.<env>.yml
-printf "services:\n" > "${HC_DC_ENV_FILE}"
+echo -e "  ${colors[Cyan]}Generate lb docker-compose config${colors[Color_Off]}"
 # LB
 {
     printf "\n";
@@ -158,9 +171,8 @@ printf "services:\n" > "${HC_DC_ENV_FILE}"
     printf "      - %s:/webroot\n" "${APPS_BASE}/lb/webroot";
     printf "      - %s:/certs\n" "${CERTIFICATE_PATH}";
 } >> ${HC_DC_ENV_FILE}
-echo ""
 
-echo "Generate MariaDB docker-compose config."
+echo -e "${colors[Cyan]}Generate MariaDB docker-compose config${colors[Color_Off]}"
 # MariaDb
 {
     printf "\n";
@@ -168,9 +180,8 @@ echo "Generate MariaDB docker-compose config."
     printf "    volumes:\n";
     printf "      - %s:/var/lib/mysql\n" "${APPS_BASE}/mariadb";
 } >> "${HC_DC_ENV_FILE}"
-echo ""
 
-echo "Generate PostgreSQL docker-compose config."
+echo -e "${colors[Cyan]}Generate PostgreSQL docker-compose config${colors[Color_Off]}"
 # PostgreSQL
 {
     printf "\n";
@@ -178,9 +189,8 @@ echo "Generate PostgreSQL docker-compose config."
     printf "    volumes:\n";
     printf "      - %s:/var/lib/postgresql/data\n" "${APPS_BASE}/postgres";
 } >> "${HC_DC_ENV_FILE}"
-echo ""
 
-echo "Generate Redis docker-compose config."
+echo -e "${colors[Cyan]}Generate Redis docker-compose config${colors[Color_Off]}"
 # Redis
 {
     printf "\n";
@@ -188,28 +198,27 @@ echo "Generate Redis docker-compose config."
     printf "    volumes:\n";
     printf "      - %s:/data\n" "${APPS_BASE}/redis";
 } >> "${HC_DC_ENV_FILE}"
-echo ""
 
-echo "Generate Authentik configurations."
+echo -e "${colors[Cyan]}Generate Authentik configurations${colors[Color_Off]}"
 AUTHENTIK_ENV_TEMPLATE="${HC_PROGRAM_PATH}"/env_files/authentik.env
 AUTHENTIK_ENV_FILE="${SERVICE_DESTINATION}"/authentik.${TARGET_ENV}
 if [ -f "${AUTHENTIK_ENV_FILE}" ]; then
     # File existed, not the first time
-    echo "   ${AUTHENTIK_ENV_FILE} exists and will not update it to avoid configuration overwrite"
-    echo "   !!! Please check and update ${AUTHENTIK_ENV_FILE} based on ${AUTHENTIK_ENV_TEMPLATE} !!!"
+    echo -e "   ${colors[Red]}${AUTHENTIK_ENV_FILE} ${colors[Yellow]}exists and will not update it to avoid configuration overwrite${colors[Color_Off]}"
+    echo -e "${colors[Yellow]}   !!! Please check and update ${AUTHENTIK_ENV_FILE} based on ${AUTHENTIK_ENV_TEMPLATE} !!!${colors[Color_Off]}"
 else
-    echo "  Copy and update env file authentik.${TARGET_ENV}"
+    echo -e "  ${colors[Cyan]}Copy and update env file authentik.${TARGET_ENV}${colors[Color_Off]}"
     cp -v "${AUTHENTIK_ENV_TEMPLATE}" "${AUTHENTIK_ENV_FILE}"
 fi
 # Authentik Server
 mkdir -p "${APPS_BASE}"/authentik/media "${APPS_BASE}"/authentik/templates "${APPS_BASE}"/authentik/data "${APPS_BASE}"/authentik/certs "${APPS_BASE}"/authentik/dist/extra
-echo "  Sync up config files."
+echo -e "  ${colors[Cyan]}Sync up config files${colors[Color_Off]}"
 rsync -ar "${HC_CONF_SOURCE_PATH}"/authentik/ "${APPS_BASE}"/authentik/
 sed -i -e "s|%PRIMARY_SERVER_NAME%|${PRIMARY_SERVER_NAME}|g" "${APPS_BASE}/authentik/data/user_settings.py"
 sed -i -e "s|%AUTHENTIK_SERVER_NAME%|${AUTHENTIK_SERVER_NAME}|g" "${APPS_BASE}/authentik/data/user_settings.py"
 sed -i -e "s|%HTTPS_PORT%|${HTTPS_PORT}|g" "${APPS_BASE}/authentik/data/user_settings.py"
 
-echo "  Generate authentik server(app) docker-compose config."
+echo -e "  ${colors[Cyan]}Generate authentik server(app) docker-compose config${colors[Color_Off]}"
 if [ "${AUTHENTIK_ENV_FILE_ENABLED}" == "yes" ]; then
 {
     printf "\n";
@@ -232,9 +241,8 @@ else
 fi
     #printf "      - %s:/web/dist/custom.css:ro\n" "${APPS_BASE}/authentik/dist/custom.css";
     #printf "      - %s:/web/dist/extra:ro\n" "${APPS_BASE}/authentik/dist/extra";
-echo ""
 
-echo "  Generate authentik worker docker-compose config."
+echo -e "  ${colors[Cyan]}Generate authentik worker docker-compose config${colors[Color_Off]}"
 # Authentikworker
 if [ "${AUTHENTIK_ENV_FILE_ENABLED}" == "yes" ]; then
 {
@@ -258,10 +266,9 @@ else
     printf "      - %s:/data/user_settings.py\n" "${APPS_BASE}/authentik/data/user_settings.py";
 } >> "${HC_DC_ENV_FILE}"
 fi
-echo ""
 
 rsync -ar "${HC_CONF_SOURCE_PATH}"/nextcloud-app/ "${APPS_BASE}"/nextcloud-app/
-echo "Generate nextcloud app docker-compose config."
+echo -e "${colors[Cyan]}Generate nextcloud app docker-compose config${colors[Color_Off]}"
 # nextcloudapp
 {
     printf "\n";
@@ -271,9 +278,8 @@ echo "Generate nextcloud app docker-compose config."
     printf "      - %s:/usr/local/etc/php-fpm.d/www.conf:ro\n" "${APPS_BASE}/nextcloud-app/www.conf";
     printf "      - %s:/var/www/html/data\n" "${DATA_BASE}/nextcloud";
 } >> "${HC_DC_ENV_FILE}"
-echo ""
 
-echo "Generate nextcloud web docker-compose config."
+echo -e "${colors[Cyan]}Generate nextcloud web docker-compose config${colors[Color_Off]}"
 # nextcloudweb
 rsync -ar "${HC_CONF_SOURCE_PATH}"/nextcloud-web/ "${APPS_BASE}"/nextcloud-web/
 {
@@ -283,9 +289,8 @@ rsync -ar "${HC_CONF_SOURCE_PATH}"/nextcloud-web/ "${APPS_BASE}"/nextcloud-web/
     printf "      - %s:/etc/nginx/nginx.conf\n" "${APPS_BASE}/nextcloud-web/nginx.conf";
     printf "      - %s:/var/www/html:ro\n" "${APPS_BASE}/nextcloud";
 } >> "${HC_DC_ENV_FILE}"
-echo ""
 
-echo "Generate nextcloud cron docker-compose config."
+echo -e "${colors[Cyan]}Generate nextcloud cron docker-compose config${colors[Color_Off]}"
 # nextcloudcron
 {
     printf "\n";
@@ -294,17 +299,16 @@ echo "Generate nextcloud cron docker-compose config."
     printf "      - %s:/var/www/html\n" "${APPS_BASE}/nextcloud";
     printf "      - %s:/var/www/html/data\n" "${DATA_BASE}/nextcloud";
 } >> "${HC_DC_ENV_FILE}"
-echo ""
 
 # Populate docker container environment variables
-echo "Generate CODE configurations."
+echo -e "${colors[Cyan]}Generate CODE configurations${colors[Color_Off]}"
 CODE_ENV_TEMPLATE="${HC_PROGRAM_PATH}"/env_files/code.env
 CODE_ENV_FILE="${SERVICE_DESTINATION}"/code.${TARGET_ENV}
-echo "  Copy and update env file code.${TARGET_ENV}"
+echo -e "  ${colors[Cyan]}Copy and update env file code.${TARGET_ENV}${colors[Color_Off]}"
 cp -v "${CODE_ENV_TEMPLATE}" "${CODE_ENV_FILE}"
 sed -i -e "s|%PRIMARY_SERVER_NAME%|${PRIMARY_SERVER_NAME}|g" "${CODE_ENV_FILE}"
 sed -i -e "s|%HTTPS_PORT%|${HTTPS_PORT}|g" "${CODE_ENV_FILE}"
-echo "  Generate CODE docker-compose config"
+echo -e "  ${colors[Cyan]}Generate CODE docker-compose config${colors[Color_Off]}"
 mkdir -p "${APPS_BASE}"/code/fonts
 
 if [ "${CODE_SERVER_ENABLED}" == "yes" ]; then
@@ -316,20 +320,18 @@ if [ "${CODE_SERVER_ENABLED}" == "yes" ]; then
     printf "      - %s:/opt/cool/systemplate/tmpfonts:rw\n" "${APPS_BASE}/code/fonts";
 } >> "${HC_DC_ENV_FILE}"
 fi
-echo ""
 
 # clamav
-echo "Generate ClamAV configuration."
+echo -e "${colors[Cyan]}Generate ClamAV configuration${colors[Color_Off]}"
 {
     printf "\n";
     printf "  clamav:\n";
     printf "    volumes:\n";
     printf "      - %s:/var/lib/clamav:rw\n" "${APPS_BASE}/clamav";
 } >> "${HC_DC_ENV_FILE}"
-echo ""
 
 # elasticsearch
-echo "Generate ElasticSearch configuration."
+echo -e "${colors[Cyan]}Generate ElasticSearch configuration${colors[Color_Off]}"
 mkdir -p "${APPS_BASE}"/elasticsearch/data
 {
     printf "\n";
@@ -337,16 +339,14 @@ mkdir -p "${APPS_BASE}"/elasticsearch/data
     printf "    volumes:\n";
     printf "      - %s:/usr/share/elasticsearch/data:rw\n" "${APPS_BASE}/elasticsearch/data";
 } >> "${HC_DC_ENV_FILE}"
-echo ""
 
 # Prepare the secrets files
-echo "Generate docker-compose secrets section"
+echo -e "${colors[Cyan]}Generate docker-compose secrets section${colors[Color_Off]}"
 echo "" >> "${HC_DC_ENV_FILE}"
 "${HC_DC_BIN_PATH}"/vault.setup.sh -a "${VAULT_BASE}" >> "${HC_DC_ENV_FILE}"
-echo ""
 
 # Generate docker-compose operators
-echo "Generate docker-compose operators."
+echo -e "${colors[Cyan]}Generate docker-compose maintenance scripts${colors[Color_Off]}"
 DC_START_DBONLY="${HC_DC_BIN_PATH}/start.dbonly.sh"
 DC_STOP_DBONLY="${HC_DC_BIN_PATH}/stop.dbonly.sh"
 DC_START="${HC_DC_BIN_PATH}/start.sh"
@@ -362,29 +362,30 @@ echo "${HC_DC_BIN_PATH}/homecloud-service.sh -c ${SERVICE_DESTINATION}/${SETUP_E
 echo "${HC_DC_BIN_PATH}/homecloud-service.sh -c ${SERVICE_DESTINATION}/${SETUP_ENV_FILENAME} -a stop -b" > "${DC_STOP_DBONLY}"
 echo "${HC_DC_BIN_PATH}/homecloud-service.sh -c ${SERVICE_DESTINATION}/${SETUP_ENV_FILENAME} -a pull" > "${DC_PULL}"
 chmod +x "${DC_START}" "${DC_STOP}" "${DC_START_DAEMON}" "${DC_START_DBONLY}" "${DC_STOP_DBONLY}" "${DC_RESTART}" "${DC_PULL}"
-echo ""
 
 # Generate create_databases.sh
-echo "Generate create_databases.sh for first time setup."
+echo -e "${colors[Cyan]}Generate Database scripts for first time setup${colors[Color_Off]}"
 CREATE_DATABASES="${HC_DC_BIN_PATH}/create_databases.sh"
 CREATE_POSTGRES="${HC_DC_BIN_PATH}/create_postgresdb.sh"
 CREATE_MARIADB="${HC_DC_BIN_PATH}/create_mariadb.sh"
 
 echo "#!/usr/bin/env bash" > "${CREATE_DATABASES}"
+# PostgreSQL
 if [ -x "${CREATE_POSTGRES}" ]; then
     echo "${CREATE_POSTGRES} -n authentik -u authentik -s ${VAULT_BASE} -d homecloud_postgres"  >> "${CREATE_DATABASES}"
     echo "${CREATE_POSTGRES} -n nextcloud -u nextcloud -s ${VAULT_BASE} -d homecloud_postgres" >> "${CREATE_DATABASES}"
 fi
-
+# MariaDB
 if [ -x "${CREATE_MARIADB}" ]; then
-    echo
+    echo "  No mariadb to create!"
 fi
 chmod +x "${CREATE_DATABASES}"
 
 if [ "$(uname)" == "Darwin" ]; then
+    echo -e "${colors[Cyan]}Clean up bak files left by Sed on MacOS${colors[Color_Off]}"
     # Fix for MacOS sed https://unix.stackexchange.com/questions/13711/differences-between-sed-on-mac-osx-and-other-standard-sed
     find "${SERVICE_DESTINATION}" -name "*\-e" -exec rm -f {} \;
     find "${APPS_BASE}" -name "*\-e" -exec rm -f {} \;
 fi
 
-echo "Prepare deployment environment ${TARGET_ENV} Done!"
+echo -e "${colors[Green]}Complete prepare HomeCloud deployment environment ${colors[Blue]}${TARGET_ENV}${colors[Color_Off]}"
