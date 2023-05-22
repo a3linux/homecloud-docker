e70c1895f98014ba3c8472d626b52b916ad170b9
Allen Chen <a3linux@gmail.com>
2023-05-22 11:50:53 +0800

    -Fix restart action

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-{{SERVICE_DESTINATION}}/bin/homecloud-service.sh -c {{SERVICE_DESTINATION}}/{{SETUP_ENV_FILENAME}} -a restart.sh
+{{SERVICE_DESTINATION}}/bin/homecloud-service.sh -c {{SERVICE_DESTINATION}}/{{SETUP_ENV_FILENAME}} -a restart
